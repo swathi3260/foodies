4175187da01f124a2a0f41caa0ffd3f799c81633
swathi3260 <swathi.peesapati@cgi.com>
2018-10-04 18:44:21 +0530

    collection card

--- src/app/displaycollections/displaycollections.component.ts
+++ src/app/displaycollections/displaycollections.component.ts
@@ -15,8 +15,7 @@ export class DisplaycollectionsComponent implements OnInit {
  // restaurantss=[];
 
   ngOnInit() {
-    // this.showCollections();
-    // this.showCollectionsfav();
+    
   }
   showCollections(){
     console.log('called showCollections');
